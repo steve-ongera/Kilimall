2367056b2417f244cdee210f11e07a987be54b9d
steve-ongera <gadafisteve001@gmail.com>
2025-02-24 01:58:11 +0300

    added context processor

--- kilimall/settings.py
+++ kilimall/settings.py
@@ -56,6 +56,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'myapp.context_processors.navbar_context',  # Add this line
+                'myapp.context_processors.categories_processor',
             ],
         },
     },
